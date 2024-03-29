2803820367b9496e14ea7ad59c5edbb4543225d5
Akshit3022 <akshit.chauhan@yudiz.com>
2024-03-29 12:30:38 +0530

    Removed unneccessary comments

--- restToDo/app/urls.py
+++ restToDo/app/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path('', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('users/', CustomUserCRUD.as_view(), name='adminDash'),
-    path('task-details/<int:id>', TaskDetails.as_view(), name='task-details'),
-    path('tasks/', TaskAPIView.as_view(), name='home'), 
-    path('tasks/<int:id>/complete/', TaskCompleteAPIView.as_view(), name='complete-task'),
-    path('tasks/<int:id>/delete/', TaskDeleteAPIView.as_view(), name='delete-task'),
-    path('logout/', LogOutAPIView.as_view(), name='logout'), 
+    path('tasks/', TaskListCreateAPIView.as_view(), name='task-list'),
+    path('tasks/<int:id>/', TaskRetrieveUpdateDestroyAPIView.as_view(), name='task-detail'),
 ]
--- restToDo/app/views.py
+++ restToDo/app/views.py
@@ -18,40 +18,6 @@ import requests
 # admin
 # RARE0000
 
-# class RegisterView(APIView):
-#     def get(self, request):
-#         # Rendering the HTML template for GET requests
-#         return render(request, 'register.html')
-#     def post(self, request):
-
-#         userName = request.data.get('userName')
-#         userEmail = request.data.get('userEmail')
-#         userPassword = make_password(request.data.get('userPassword'))
-
-        # url = requests.get('').json()
-
-        # if not userName or not userEmail or not userPassword:
-        #     return Response({'error': 'Please provide username, email, and password'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # if CustomUser.objects.filter(userName=userName).exists():
-        #     return Response({'error': 'Username is already taken'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # if CustomUser.objects.filter(userEmail=userEmail).exists():
-        #     return Response({'error': 'Email is already registered'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # user = CustomUser.objects.create(userName=userName, userEmail=userEmail, userPassword=userPassword)
-        # user.save()
-        # return redirect('login')
-
-        # serializer = CustomUserSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     password = make_password(serializer.validated_data['userPassword'])
-        #     serializer.validated_data['userPassword'] = password
-        #     serializer.save()
-        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        # return render(request, 'register.html') 
-
 
 class RegisterView(APIView):
     # def post(self, request):
@@ -75,8 +41,6 @@ class RegisterView(APIView):
             userEmail = request.data.get('userEmail')
             userPassword = make_password(request.data.get('userPassword'))
 
-            url = requests.get('').json()
-
             if not userName or not userEmail or not userPassword:
                 return Response({'error': 'Please provide username, email, and password'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -87,9 +51,7 @@ class RegisterView(APIView):
                 return Response({'error': 'Email is already registered'}, status=status.HTTP_400_BAD_REQUEST)
 
             user = CustomUser.objects.create(userName=userName, userEmail=userEmail, userPassword=userPassword)
-            # user.save()
             if user:
-                # return render(request, 'register.html')
                 return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
             else:
                 return Response({'error': 'Failed to create user'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -111,6 +73,7 @@ class LoginView(APIView):
     def post(self, request):
         userName = request.data.get('userName')
         userPassword = request.data.get('userPassword')
+        request.session['username'] = userName
         print("userPassword", userPassword)
         user = authenticate(username=userName, password=userPassword)
         print("USER", user)
@@ -123,156 +86,44 @@ class LoginView(APIView):
         else:
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
-        # try:
-        #     loginUser = CustomUser.objects.get(userEmail=userEmail)
-        # except Exception as e:
-        #     loginUser = None
-
-        # if CustomUser.objects.filter(userEmail=userEmail).exists() and check_password(userPassword, loginUser.userPassword):
-        #     request.session['email'] = userEmail
-        #     return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
-        # else:
-        #     return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-    
-    # def get(self, request):
-    #     return render(request, 'login.html')
-    
-    # def post(self, request):
-
-    #     userEmail = request.data.get('userEmail')
-    #     userPassword = request.data.get('userPassword')
-
-    #     try:
-    #         loginUser = CustomUser.objects.get(userEmail=userEmail)
-    #     except Exception as e:
-    #         loginUser = None
-
-        # user = request.user
-        # print("user", user)
-
-        # print("check_password", check_password(userPassword, loginUser.userPassword))
-        # if loginUser is not None and check_password(userPassword, loginUser.userPassword):
-        #     print("-------------------------------")
-        #     request.session['email'] = userEmail
-        #     return redirect('home')
-        # elif userEmail=='admin@gamil.com':
-        #     request.session['email'] = userEmail
-        #     return redirect('adminDash')
-        # else:
-        #     request.session['email'] = userEmail
-        #     return redirect('home')
-        #     return render(request,'login.html')
-
-
 
 class CustomUserCRUD(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
-    def get(self, request):
-        allUser = CustomUser.objects.all()
-        return render(request, 'adminDash.html', {'allUser':allUser})
-
-# class TaskListCreateAPIView(APIView):
-#     queryset = Task.objects.all()
-#     serializer_class = TaskSerializer
-#     # permission_classes = [IsAuthenticated]
-
-#     def get(self, request):
-#         logedUserTask = Task.objects.filter(user_id__userEmail=request.session['email'])
-#         return render(request, 'home.html', {'logedUserTask':logedUserTask})
-
-#     def post(self, request):
-#         taskDate = datetime.datetime.now()
-#         taskName = request.data.get('taskName')
-#         taskDescription = request.data.get('taskDescription')
-#         loggedUser = request.session['email']
-#         user_instance = CustomUser.objects.get(userEmail=loggedUser)
-
-#         task = Task(user_id=user_instance, taskDate=taskDate, taskName=taskName, taskDescription=taskDescription)
-#         task.save()
-#         return redirect ('home')
-
-
-class LogOutAPIView(APIView):
-    def get(self, request):
-        del request.session['email']
-        return redirect('login')
-
-# class TaskDestroyAPIView(APIView):
-#     queryset = Task.objects.all()
-#     serializer_class = TaskSerializer
-#     # permission_classes = [IsAuthenticated]
-
-#     def post(self, request, id):
-#         Task.objects.filter(userEmail=request.session['email'])
-#         return render(request, 'home.html')
     
-#     def patch(self, request, id):
-
-#         task = Task.objects.get(id=id)
-#         task.taskStatus = True
-
-#         status = "Completed"
-#         return render(request, 'home.html', {'status': status})
-
-#     def delete(self, request, id):
-
-#         task = Task.objects.get(id=id)
-#         task.delete()
-#         return redirect('home')
-
-
-class TaskAPIView(APIView):
+class TaskListCreateAPIView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
-    def get(self, request):
-        # Retrieve tasks for the logged-in user
-        logedUserTask = Task.objects.filter(user_id__userEmail=request.session.get('email'))
-        return render(request, 'home.html', {'logedUserTask': logedUserTask})
+    # permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        # Create a new task
         taskDate = datetime.datetime.now()
         taskName = request.data.get('taskName')
         taskDescription = request.data.get('taskDescription')
-        loggedUser = request.session.get('email')
-        user_instance = CustomUser.objects.get(userEmail=loggedUser)
+        loggedUser = request.session['username']
+        user_instance = CustomUser.objects.get(userName=loggedUser)
 
-        task = Task(user_id=user_instance, taskDate=taskDate, taskName=taskName, taskDescription=taskDescription)
-        task.save()
-        return redirect('home')
+        try:
+            task = Task(user_id=user_instance, taskDate=taskDate, taskName=taskName, taskDescription=taskDescription)
+            task.save()
+            return Response({'message': 'Task created successful'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            print("error",e)
+            return Response({'error': 'Task creation failed'}, status=status.HTTP_400_BAD_REQUEST)
 
-    # def patch(self, request, id):
-    #     # Update task status to completed
-    #     task = Task.objects.get(id=id)
-    #     task.taskStatus = True
-    #     task.save()
-    #     status = "Completed"
-    #     return render(request, 'home.html', {'status': status})
 
-    # def delete(self, request, id):
-    #     # Delete task
-    #     task = Task.objects.get(id=id)
-    #     task.delete()
-    #     return redirect('home')
-class TaskDetails(APIView):
-    def get(self, request, id):
-        tasks = Task.objects.filter(user_id__user_id=id)
-        return render(request, 'displayTask.html', {'tasks': tasks})    
+class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    # permission_classes = [IsAuthenticated]
 
-class TaskCompleteAPIView(APIView):
-    def post(self, request, id):
-        # Update task status to completed
-        task = Task.objects.get(task_id=id)
-        task.taskStatus = True
-        task.save()
-        return redirect('home')
-    
-class TaskDeleteAPIView(APIView):
-    def post(self, request, id):
-        # Delete task
-        task = Task.objects.get(task_id=id)
-        task.delete()
-        return redirect('home')
+
+    def delete(self, request, id):
+        try:
+            task = Task.objects.get(id=id)
+            task.delete()
+            return Response({'message': 'Task deleted successful'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            print("error",e)
+            return Response({'error': 'Task deletion failed'}, status=status.HTTP_400_BAD_REQUEST)
